570f9fe25cb7676c2c627fc7f3738990d139256d
bangkitdwijaya <119310260+bangkitdwijaya@users.noreply.github.com>
2022-12-04 12:53:01 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By Screenesia         #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Screenesia Digital                #"
+echo -e "================================================="
+
